ef793d5203b3dfce73daf4f339819123760d134c
Humnansingle <144996279+Humnansingle@users.noreply.github.com>
2023-10-16 12:56:25 +0530

    v1

---
+++ data.js
@@ -0,0 +1,39 @@
+const servicesData=[
+    {
+        title:"Frontend Development",
+        category:"frontend",
+        description:['frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?',
+        'frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit? frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?',
+        
+        'frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?']
+          
+             
+        
+    
+    },
+    {
+        title:"Backend Development",
+        category:"backend",
+        description:['backend  Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?',
+
+        'frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit? frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?',
+
+        'frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?'
+          
+        ]
+    
+    },
+    {
+        title:"  FullStack Development",
+        category:"fullstack",
+        description:[
+            'fullstack Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?',
+
+            'frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit? frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?',
+
+            'frontend Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, exercitationem laboriosam odio delectus corrupti ad minus error vitae rerum consequuntur odit architecto eius nemo, facilis ullam fugiat. Provident, at reprehenderit?'
+        ]
+    
+    }
+]
+
